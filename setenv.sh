e968458b3e61615dece7ba47b13b9d62f0b33f6b
Florian Eble <florian.eble@cern.ch>
2024-02-20 18:23:17 +0100

    Adding lxplus 9 support

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,13 @@
-export LCG_RELEASE_DIR=/cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc8-opt
+lxplus7_regex="lxplus7"
+lxplus9_regex="lxplus9"
+
+if [[ $HOSTNAME =~ ${lxplus9_regex} ]]; then
+  export LCG_BASE_PATH=/cvmfs/sft.cern.ch/lcg/views/LCG_105/x86_64-el9-clang16-dbg
+elif  [[ $HOSTNAME =~ ${lxplus7_regex} ]]; then
+  export LCG_BASE_PATH=/cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc8-opt
+else
+  echo "Unsupported hostname ${HOSTNAME}". Only supporting lxplus 7 and 9.
+fi
 
 export PIXEL_MONITORING_DIR=${PWD}
 
